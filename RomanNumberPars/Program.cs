b7c306ce19e687f96551a8fe2eddddc1f98e26e5
0WhyMe0 <wolodymyrpelekh@gmail.com>
2024-05-28 14:34:27 +0200

    Add project files.

---
+++ RomanNumberPars/Program.cs
@@ -0,0 +1,11 @@
+﻿using RomanNumberPars;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        RomNumber romNumber = new RomNumber();
+
+        Console.WriteLine(romNumber.Parse("IV"));
+    }
+}
---
+++ RomanNumberPars/RomNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumberPars
+{
+    internal class RomNumber
+    {
+        Dictionary<char, int> RomanNumber = new Dictionary<char, int>()
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+        
+        public int Parse(string RomanNumberParse)
+        {
+            int result = 0;
+
+            for (int i = 0; i < RomanNumberParse.Length; i++)
+            {
+                if(i + 1 < RomanNumberParse.Length && RomanNumberParse[i] < RomanNumberParse[i + 1])
+                {
+                    result -= RomanNumber[RomanNumberParse[i]];
+                }
+                else
+                {
+                    result += RomanNumber[RomanNumberParse[i]];
+                }
+            }
+            return result;
+        }
+    }
+}
